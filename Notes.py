04d1ed79c514c09ef647bb211ad0813669de9f23
ioqv <34071887+ioqv@users.noreply.github.com>
2017-11-29 10:48:58 -0800

    added function
    Nov-29-17

--- Notes.py
+++ Notes.py
@@ -1,17 +1,77 @@
-print(15 % 5)
-print(14 % 5)
-print(13 % 5)
-print(12 % 5)
-print(11 % 5)
-print(10 % 5)
-print(9 % 5)
-print(5 % 3)
+    # print(15 % 5)
+    # print(14 % 5)
+    # print(13 % 5)
+    # print(12 % 5)
+    # print(11 % 5)
+    # print(10 % 5)
+    # print(9 % 5)
+    # print(5 % 3)
+    #
+    #
+    # car_name = "Weibe Mobile"
+    # car_type = "Nissan GTR"
+    # car_cylinders = 8
+    # car_mpg = 9000.1
+    #
+# # Inline printing
+ # print("I have a car called the %s" % car_name)
 
 
-car_name = "Weibe Mobile"
-car_type = "Nissan GTR"
-car_cylinders = 8
-car_mpg = 9000.1
+    # Functions
 
-# Inline printing
-print("I have a car called the %s" % car_name)
+
+def print_hw():
+    print("Hello World")
+
+
+print_hw()
+print_hw()
+print_hw()
+
+def say_hi(name): # name is a parameter
+    print("Hello %s." % name)
+    print("Enjoy your day.")
+
+
+say_hi("John")
+
+
+def print_age(name, age):
+    print("%s is %d years old." % (name, age))
+    age = age + 1 # age = age + 1
+    print("Next year, they will be %d" % age)
+
+
+print_age("John", 15)
+
+def f(x):
+    return x**3 + 4 * x**2 + 7 * x - 4
+
+
+print(f(3))
+print(f(4))
+print(f(5))
+
+
+# If staments
+
+
+def grade_calc(percentage):
+    if percentage >= 90:
+        return "A"
+    elif percentage < 90 and percentage >=80 :
+        return "B"
+    elif percentage >= 70:
+        return"C"
+    elif percentage >= 60:
+        return"D"
+    elif percentage >= 50:
+        return"E"
+     
+
+def grade_calc(percentage):
+    if
+
+
+
+
